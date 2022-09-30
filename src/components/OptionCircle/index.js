8b9b4fd6e1b0cd01f4d353ff8a86d2d44de692e1
unlocker1205 <19130215@st.hcmuaf.edu.vn>
2022-09-30 13:47:29 +0700

    Fix option circle

--- src/components/OptionCircle/index.js
+++ src/components/OptionCircle/index.js
@@ -8,20 +8,26 @@ import AddToWatchListIcon from '~/assets/images/AddToWatchListIcon';
 import HeaderIcon from '~/assets/images/HeaderIcon';
 import StartIcon from '~/assets/images/StartIcon';
 
-function OptionCircle({ handleClickOption }) {
+function OptionCircle() {
     return (
         <div
-            onClick={handleClickOption}
-            className="option absolute top-2 right-2 w-[1.5em] h-[1.5em] text-[1em] cursor-pointer "
+            onClick={(e) => e.stopPropagation()}
+            className="option absolute top-2 right-2 w-[1.5em] h-[1.5em] text-[1em] cursor-pointer z-10"
         >
             <Tippy
                 trigger="click"
-                delay={[0, 450]}
+                delay={[0, 400]}
                 interactive="true"
-                placement="bottom"
+                placement="bottom-end"
+                onMount={(instance) => {
+                    instance.setProps({ trigger: 'mouseenter focus' });
+                }}
+                onHide={(instance) => {
+                    instance.setProps({ trigger: 'click' });
+                }}
                 render={(attrs) => (
                     <PopperWrapper>
-                        <div className="flex flex-col items-start justify-center w-full" tabIndex="-1" {...attrs}>
+                        <div className="flex flex-col items-start justify-center w-full z-10" tabIndex="-1" {...attrs}>
                             <PopperItem iconLeft={<AddToListIcon />}>Add to list</PopperItem>
                             <PopperItem iconLeft={<HeaderIcon />}>Favorite</PopperItem>
                             <PopperItem iconLeft={<AddToWatchListIcon />}>Watchlist</PopperItem>
